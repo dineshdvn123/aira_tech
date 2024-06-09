dd83c1ec79a5ff1802ca245db444c91ecbe6dde2
venkatanagadinesh <venkatanagadineshkumar.desai@gwmail.gwu.edu>
2024-06-08 22:02:20 -0400

    navigation_issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import React, {useEffect} from 'react';
+import { Route, Routes, useNavigate } from "react-router-dom";
 import NavBar from './components/NavBar/NavBar';
 import LandingPage from './components/pages/LandingPage';
 import ContactUs from './components/ContactUs/ContactUs'
 import Footer from './components/Footer/Footer'
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, [navigate]);
+  
   return (
     <div className="App">
       <NavBar />
