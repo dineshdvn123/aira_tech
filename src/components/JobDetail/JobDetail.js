73cf41470ceeb4aa5c8df079958f8df0dc706c29
venkatanagadinesh <134567601+dineshdvn123@users.noreply.github.com>
2024-06-27 18:39:14 -0400

    Update url

--- src/components/JobDetail/JobDetail.js
+++ src/components/JobDetail/JobDetail.js
@@ -75,7 +75,7 @@ function JobDetail() {
     const fetchJob = async () => {
       try {
         // const response = await axios.get(`http://localhost:8080/jobs/${jobId}`);
-        const response = await axios.get(`https://aira-tech-backend-1.onrender.com/${jobId}`);
+        const response = await axios.get(`https://aira-tech-backend-1.onrender.com/jobs/${jobId}`);
         setJob(response.data);
         setLoading(false);
       } catch (err) {
